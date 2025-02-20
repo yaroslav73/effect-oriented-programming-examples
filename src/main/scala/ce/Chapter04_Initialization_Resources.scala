8300e7ed805b460f8429a63fef5bcc36793072b3
Yaroslav Sydorenko <ysydorenko@evolution.com>
2025-02-20 14:40:36 +0200

    Add examples with Resources.

---
+++ src/main/scala/ce/Chapter04_Initialization_Resources.scala
@@ -0,0 +1,198 @@
+package ce
+
+import cats.effect.{IO, Ref, Resource}
+import ce.extensions.*
+import pureconfig.*
+import pureconfig.generic.derivation.*
+
+trait Bread:
+  val eat: IO[Unit] = IO.println("Bread: Eating")
+
+class BreadStoreBought extends Bread
+
+val purchaseBread =
+  IO.println("Buying bread").as(BreadStoreBought())
+
+val storeBoughtBread =
+  Resource.eval(purchaseBread)
+
+val eatBread: Bread => IO[Unit] =
+  bread => bread.eat
+
+object App0 extends ce.helpers.IOAppDebug:
+  def run: IO[Any] =
+    storeBoughtBread.use(eatBread)
+
+class Dough:
+  val letRise: IO[Unit] = IO.println("Dough: rising")
+
+object Dough:
+  val fresh: Resource[IO, Dough] =
+    Resource.eval(IO.println("Dough: Mixed").as(Dough()))
+
+trait HeatSource
+
+class Oven extends HeatSource
+
+object Oven:
+  val heated: Resource[IO, Oven] =
+    Resource.eval(IO.println("Oven: Heated").as(Oven()))
+
+class BreadHomeMade(heat: HeatSource, dough: Dough) extends Bread
+
+object BreadHomeMade:
+  def make(heat: Oven, dough: Dough): Resource[IO, BreadHomeMade] =
+    for
+      _ <- IO.println("BreadHomeMade: Baked").toResource
+    yield BreadHomeMade(heat, dough)
+
+//val homeMadeBread = BreadHomeMade.make(Oven.heated, Dough.fresh)
+
+object App1 extends ce.helpers.IOAppDebug:
+  def run: IO[Any] =
+    val breadHomeMade = for
+      oven <- Oven.heated
+      dough <- Dough.fresh
+      bread <- BreadHomeMade.make(oven, dough)
+    yield bread
+
+    breadHomeMade.use(eatBread)
+
+object Bread:
+  val storeBought: Resource[IO, BreadStoreBought] = storeBoughtBread
+  val homeMade: (Oven, Dough) => Resource[IO, BreadHomeMade] =
+    (oven, dough) => BreadHomeMade.make(oven, dough)
+
+trait Toast:
+  val bread: Bread
+  val heat: HeatSource
+  val eat: IO[Unit] = IO.println("Toast: Eating")
+
+final case class ToastFromHeatSource(bread: Bread, heat: HeatSource) extends Toast
+
+object ToastFromHeatSource:
+  val toasted: (IO[HeatSource], IO[Bread]) => IO[ToastFromHeatSource] =
+    (heat, bread) =>
+      for
+        bread <- bread
+        heat <- heat
+        _ <- IO.println("Toast: Made")
+      yield ToastFromHeatSource(bread, heat)
+
+class Toaster extends HeatSource
+
+object Toaster:
+  val ready: Resource[IO, Toaster] = Resource.eval(IO.println("Toaster: Ready").as(Toaster()))
+
+final case class ToastFromToaster(bread: Bread, heat: HeatSource) extends Toast
+
+object ToastFromToaster:
+  val toasted: (Bread, HeatSource) => Resource[IO, ToastFromToaster] =
+    (bread, heat) =>
+      for
+        _ <- IO.println("Toast: Made").toResource
+      yield ToastFromToaster(bread, heat)
+
+object App2 extends ce.helpers.IOAppDebug:
+  def run: IO[Any] =
+    val homeMadeToast = for
+      toaster <- Toaster.ready
+      dough <- Dough.fresh
+      heat <- Oven.heated
+      bread <- Bread.homeMade(heat, dough)
+      toast <- ToastFromToaster.toasted(bread, toaster)
+    yield toast
+
+    homeMadeToast.use(_.eat)
+
+object OvenSafe:
+  val heated: Resource[IO, Oven] =
+    Resource.make(IO.println("Oven: Heated").as(Oven()))(_ => IO.println("Oven: Turning off"))
+
+// Skip App3 zio, it's the same as App2 but with ZLayer.Debug.Tree
+object App3 extends ce.helpers.IOAppDebug:
+  def run: IO[Any] =
+    val homeMadeBread = for
+      heat <- OvenSafe.heated
+      dough <- Dough.fresh
+      bread <- Bread.homeMade(heat, dough)
+    yield bread
+
+    homeMadeBread.use(eatBread)
+
+//class BreadFromFriend extends Bread
+//
+//object Friend:
+//  def forcedFailure(invocations: Int): IO[BreadFromFriend] =
+//    IO.println(s"Attempt $invocations: Failure(Friend Unreachable)") *>
+//      IO.raiseError(new Exception("Friend Unreachable"))
+//        .as(BreadFromFriend())
+//
+//  def requestBread(retry: Ref[IO, Int]): IO[BreadFromFriend] =
+//    val worksOnAttempt = 4
+//    for
+//      curInvocations <- retry.updateAndGet(_ + 1)
+//      bread <- if curInvocations < worksOnAttempt then forcedFailure(curInvocations)
+//      else IO.println(s"Attempt $curInvocations: Succeeded").as(BreadFromFriend())
+//    yield bread
+//  end requestBread
+//end Friend
+//
+//object App4 extends ce.helpers.IOAppDebug:
+//  def run: IO[Any] =
+//    for
+//      retry <- Ref.of[IO, Int](0)
+//      bread = Friend.requestBread(retry)
+//      _ <- eatBread(bread)
+//    yield ()
+//
+//object App5 extends ce.helpers.IOAppDebug:
+//  def run: IO[Any] =
+//    for
+//      retry <- Ref.of[IO, Int](0)
+//      bread = Friend.requestBread(retry).orElse(Bread.storeBought)
+//      _ <- eatBread(bread)
+//    yield ()
+//
+//object App6 extends ce.helpers.IOAppDebug:
+//  def run: IO[Any] =
+//    for
+//      retry <- Ref.of[IO, Int](0)
+//      bread = Friend.requestBread(retry)
+//      _ <- eatBread(bread).retryN(1)
+//    yield ()
+//
+//final case class RetryConfig(times: Int)derives ConfigReader
+//
+//val configurableBread: (Ref[IO, Int], RetryConfig) => IO[Bread] =
+//  (retry, config) => Friend.requestBread(retry).retryN(config.times)
+//
+//object App7 extends ce.helpers.IOAppDebug:
+//  val retryTwice: RetryConfig = RetryConfig(2)
+//
+//  def run: IO[Any] =
+//    for
+//      retry <- Ref.of[IO, Int](0)
+//      bread = configurableBread(retry, retryTwice)
+//      _ <- eatBread(bread)
+//    yield ()
+//
+//val configSource =
+//  ConfigSource
+//    .string("{ times: 3 }")
+//    .load[RetryConfig]
+//
+//val configuration =
+//  configSource match
+//    case Right(config) => IO(config)
+//    case Left(failure) => IO.raiseError(new Exception(failure.toString))
+//
+//object App8 extends ce.helpers.IOAppDebug:
+//  def run: IO[Any] =
+//    for
+//      retry <- Ref.of[IO, Int](0)
+//      config <- configuration
+//      bread = configurableBread(retry, config)
+//      _ <- eatBread(bread)
+//    yield ()
+
