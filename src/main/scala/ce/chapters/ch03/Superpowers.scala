7d15ee8ac0394bfe0eaa281324507405da8fed9f
Yaroslav Sydorenko <ysydorenko@evolution.com>
2025-03-13 16:53:55 +0200

    Add scalafmt. Update style in Superpowers.scala

--- src/main/scala/ce/chapters/ch03/Superpowers.scala
+++ src/main/scala/ce/chapters/ch03/Superpowers.scala
@@ -17,28 +17,11 @@ enum Scenario:
 
   def simulate[A](effect: IO[A]): IO[A] =
     for
-      _ <- IO {
-        currentScenario = this
-      }
+      _      <- IO { currentScenario = this }
       result <- effect
     yield result
-//    this match
-//      case Successful =>
-//        effect
-//      case NeverWorks =>
-//        IO.raiseError(new Exception("Never works"))
-//      case Slow =>
-//        IO.sleep(100.millis) *> effect
-//      case WorksOnTryInner(ref) =>
-//        ref.get.flatMap {
-//          case 2 => effect
-//          case _ => ref.update(_ + 1) *> IO.raiseError(new Exception("Try again"))
-//        }
 
 object Scenario:
-  //  def WorksOnThirdTry: IO[WorksOnTryInner] =
-  //    Ref.of[IO, Int](2).map(WorksOnTryInner(_))
-
   def WorksOnThirdTry: Scenario =
     Ref.of[IO, Int](2).map(WorksOnTryInner(_)).unsafeRunSync()
 
@@ -47,23 +30,23 @@ def saveUser(username: String): IO[String] =
 
   val fail: IO[String] =
     IO.raiseError(new Exception("**Database crashed!!**"))
-      .handleErrorWith(error =>
+      .handleErrorWith { error =>
         IO.println(s"Log: ${error.getMessage}") *>
           IO.raiseError(error)
-      )
+      }
 
   for
     scenario <- IO(currentScenario)
-    _ <- IO.println("Attempting to save user")
-    result <- scenario match
-      case Scenario.NeverWorks => fail
-      case Scenario.Successful => succeed
-      case Scenario.Slow => IO.sleep(10.seconds) *> succeed
-      case Scenario.WorksOnTryInner(ref) =>
-        ref.get.flatMap {
-          case 0 => succeed
-          case _ => ref.update(_ - 1) *> fail
-        }
+    _        <- IO.println("Attempting to save user")
+    result   <- scenario match
+                  case Scenario.NeverWorks           => fail
+                  case Scenario.Successful           => succeed
+                  case Scenario.Slow                 => IO.sleep(10.seconds) *> succeed
+                  case Scenario.WorksOnTryInner(ref) =>
+                    ref.get.flatMap {
+                      case 0 => succeed
+                      case _ => ref.update(_ - 1) *> fail
+                    }
   yield result
 end saveUser
 
@@ -77,27 +60,32 @@ val userName = "Morty"
 
 val effect0: IO[String] = saveUser(userName)
 
+import Scenario.*
+
 object MyApp extends IOApp.Simple:
   def run: IO[Unit] =
-    Scenario.Successful.simulate(effect0).void
+    Successful.simulate:
+      effect0.void
 
 object App0 extends IOAppDebug:
   def run: IO[Any] =
-    Scenario.Successful.simulate(effect0)
+    Successful.simulate(effect0)
 
 object App1 extends IOAppDebug:
   def run: IO[Any] =
-    Scenario.WorksOnThirdTry.simulate(effect0)
+    WorksOnThirdTry.simulate:
+      effect0
 
 val effect1 = effect0.retryN(2)
 
 object App2 extends IOAppDebug:
   def run: IO[Any] =
-    Scenario.WorksOnThirdTry.simulate(effect1)
+    WorksOnThirdTry.simulate:
+      effect1
 
 object App3 extends IOAppDebug:
   def run: IO[Any] =
-    Scenario.NeverWorks.simulate:
+    NeverWorks.simulate:
       effect1
 
 val effect2 =
@@ -117,37 +105,45 @@ val effect3 =
 
 object App5 extends IOAppDebug:
   def run: IO[Any] =
-    Scenario.Slow.simulate(effect3)
+    Slow.simulate:
+      effect3
 
 val effect4 =
-  effect3.orElse(sendToManualQueue(userName))
+  effect3.orElse:
+    sendToManualQueue:
+      userName
 
 object App6 extends IOAppDebug:
   def run: IO[Any] =
-    Scenario.NeverWorks.simulate(effect4)
+    NeverWorks.simulate:
+      effect4
 
 val effect5 =
-  effect4.guarantee(logUserSignup(userName))
+  effect4.guarantee:
+    logUserSignup(userName)
 
 object App7 extends IOAppDebug:
   def run: IO[Any] =
-    Scenario.Successful.simulate(effect5)
+    Successful.simulate:
+      effect5
 
 val effect6 = effect5.timed
 
 object App8 extends IOAppDebug:
   def run: IO[Any] =
-    Scenario.Successful.simulate(effect6)
+    Successful.simulate:
+      effect6
 
 val effect7 = effect6.when(userName != "Morty")
 
 object App9 extends IOAppDebug:
   def run: IO[Any] =
-    Scenario.Successful.simulate(effect7)
+    Successful.simulate:
+      effect7
 
 object App10 extends IOAppDebug:
   def run: IO[Any] =
-    Scenario.Successful.simulate:
+    Successful.simulate:
       IO.println("Before save")
       effect1
 
@@ -156,12 +152,12 @@ val effect8 =
 
 object App11 extends IOAppDebug:
   def run: IO[Any] =
-    Scenario.Successful.simulate:
+    Successful.simulate:
       effect8
 
 object App12 extends IOAppDebug:
   def run: IO[Any] =
-    Scenario.Successful.simulate:
+    Successful.simulate:
       IO.println("**Before**") *>
         effect8.debug().repeatN(1) *>
         IO.println("**After**")
